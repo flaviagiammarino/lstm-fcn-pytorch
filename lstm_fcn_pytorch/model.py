fix softmax

lstm_fcn_pytorch/model.py
@@ -108,7 +108,7 @@ class Model():
                 loss = loss_fn(outputs, targets)
                 loss.backward()
                 optimizer.step()
-                accuracy = (torch.argmax(outputs, dim=-1) == targets).float().sum() / targets.shape[0]
+                accuracy = (torch.argmax(torch.nn.functional.softmax(outputs, dim=-1), dim=-1) == targets).float().sum() / targets.shape[0]
             if verbose:
                 print('epoch: {}, loss: {:,.6f}, accuracy: {:.6f}'.format(1 + epoch, loss, accuracy))
         self.model.train(False)
@@ -134,7 +134,7 @@ class Model():
         x = (x - self.x_min) / (self.x_max - self.x_min)
         
         # Get the predicted probabilities.
-        p = self.model(torch.from_numpy(x).to(self.device).float())
+        p = torch.nn.functional.softmax(self.model(torch.from_numpy(x).to(self.device).float()), dim=-1)
 
         # Get the predicted labels.
         y = np.argmax(p.detach().cpu().numpy(), axis=-1)

lstm_fcn_pytorch/modules.py
@@ -141,7 +141,6 @@ class LSTM_FCN(torch.nn.Module):
         self.fcn = FCN(filters, kernel_sizes)
         self.lstm = LSTM(input_length, units, dropout)
         self.linear = torch.nn.Linear(in_features=filters[-1] + units[-1], out_features=num_classes)
-        self.softmax = torch.nn.Softmax(dim=-1)
     
     def forward(self, x):
         
@@ -155,14 +154,13 @@ class LSTM_FCN(torch.nn.Module):
         Returns:
         __________________________________
         y: torch.Tensor.
-            Predicted probabilities, tensor with shape (samples, num_classes) where samples is the number
-            of time series and num_classes is the number of classes.
+            Logits, tensor with shape (samples, num_classes) where samples is the number of time series
+            and num_classes is the number of classes.
         '''
         
         x = torch.reshape(x, (x.shape[0], 1, x.shape[1]))
         y = torch.concat((self.fcn(x), self.lstm(x)), dim=-1)
         y = self.linear(y)
-        y = self.softmax(y)
         
         return y
 